819eb6209da5e9a00743949fdca9c7044dd58556
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-19 09:31:07 +0500

    Resolving Code Errors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ alert("Welcome to Number Guessing Game");
 let userLimit = +prompt("Enter the limit of Guess Number from 1 to 100");
 let systemGenertedNum = Math.floor(Math.random() * userLimit);
 console.log(systemGenertedNum);
-alert(`Guess the number from 0 to ${userLimit} `);
+alert(`Guess the number from 0 to ${userLimit - 1} `);
 if (userLimit != NaN) {
     let userInputNum = +prompt("Enter your guess");
     console.log(userInputNum);
@@ -13,7 +13,8 @@ if (userLimit != NaN) {
 
         while (userInputNum != systemGenertedNum) {
             alert('Sorry! Wrong Guess');
-            userInputNum = +prompt("Guess the number again")
+            userInputNum = +prompt("Guess the number again");
+            console.log(userInputNum);
             if (userInputNum === systemGenertedNum) {
                 alert("Hurrah! You have guess the Correct Number")
             }
