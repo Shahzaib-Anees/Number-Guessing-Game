2701590caa4761acc3a410e2e01f3093d53c78c9
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-03-19 09:21:09 +0500

    Code for game added

---
+++ index.js
@@ -0,0 +1,22 @@
+
+alert("Welcome to Number Guessing Game");
+let userLimit = +prompt("Enter the limit of Guess Number from 1 to 100");
+let systemGenertedNum = Math.floor(Math.random() * userLimit);
+console.log(systemGenertedNum);
+alert(`Guess the number from 0 to ${userLimit} `);
+if (userLimit != NaN) {
+    let userInputNum = +prompt("Enter your guess");
+    console.log(userInputNum);
+    if (userInputNum === systemGenertedNum) {
+        alert("Hurrah! You have guess the Correct Number")
+    } else {
+
+        while (userInputNum != systemGenertedNum) {
+            alert('Sorry! Wrong Guess');
+            userInputNum = +prompt("Guess the number again")
+            if (userInputNum === systemGenertedNum) {
+                alert("Hurrah! You have guess the Correct Number")
+            }
+        }
+    }
+}
